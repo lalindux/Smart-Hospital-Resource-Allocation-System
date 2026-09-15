31cda196c4d7255b74b0b599177ea45c5b31afc7
lalindux <lalindux@gmail.com>
2026-09-15 23:44:40 +0530

    Create main variable and constant

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PATIENTS 1000
+#define SPECIALTIES 4
+#define WARD 4
+
+
+const char SPECIALTY_NAMES[5][30] = {"", "General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"};
+const double BASE_FEES[]          = {0.0, 1500.00, 2500.00, 4500.00, 5000.00};
+const int AVG_TIME[]              = {0, 15, 20, 30, 30};
+
+const char WARD_NAMES[5][30]      = {"", "General Ward", "Paediatric Ward", "Surgical Ward", "ICU (Intensive Care Unit)"};
+const double WARD_DAILY_RATES[]   = {0.0, 3000.00, 6000.00, 12000.00, 25000.00};
 int main()
 {
+    char patientName[PATIENTS][50];
+    int age[PATIENTS];
+    int triageLevel[PATIENTS];
+    int specialtyID[PATIENTS];
+    int isAdmitted[PATIENTS];
+    int wardID[PATIENTS];
+    int daysAdmitted[PATIENTS];
+
+    double totalAmount[PATIENTS];
+    double TotalRevenue = 0.0;
+    double TotalDiscounts = 0.0;
 
     int choice;
     do {
